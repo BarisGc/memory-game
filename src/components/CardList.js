51147eb4b7f9380377e696475e3530c7abb27be1
BarisGc <baris.devrim1@gmail.com>
2022-04-07 17:29:21 +0300

    3) Cardlist Flipping cards(WIP)

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,14 +1,19 @@
-import { useEffect, useMemo, useLayoutEffect } from 'react'
+import { useEffect, useState, useMemo, useLayoutEffect } from 'react'
 import { useSelector, useDispatch, } from 'react-redux';
 import { toggleCards } from '../redux/memoryCardsSlice'
 import { Row, Col, Card, ListGroup } from 'react-bootstrap'
 
 function CardList() {
     const dispatch = useDispatch();
+    //states
     const cards = useSelector((state) => state.memoryCards.items);
 
     console.log("cards", cards)
 
+    const handleToggleCards = (card) => {
+        dispatch(toggleCards({ id: card.id }))
+
+    }
 
     // useEffect(() => {
     //     //preloading image
@@ -20,20 +25,32 @@ function CardList() {
 
     return (
         <>
-            <Row xs={2} md={4} className="g-1 memoryCards mb-3">
+            <Row xs={2} md={4} className="g-1 memoryCards mb-3 justify-content-center">
                 {cards.map((card) => (
-                    <Col key={card.id} >
-                        <Card className='position-relative '>
-                            <Card.Img variant="top" src={card.isOpen === true ? card.imageUrl : '/Images/png/questionmark.png'} className='mx-auto mt-2' />
-                            <Card.Body className='p-1'>
-                                <div className='cardFlipper stretched-link' onClick={() => dispatch(toggleCards({ id: card.id }))}>
-                                    <Card.Title className="memoryCardsTitle ">{card.isOpen === true ? card.name : 'Click To Flip'}</Card.Title>
-                                </div>
-                            </Card.Body>
+                    <Col key={card.id} className='flip-card'>
+                        <Card className={`position-relative flip-card-inner ${card.isOpen ? 'active' : 'notActive'}`}>
+                            <div className={`flip-card-front `}>
+                                {/* // className={item.completed ? "completed" : ""}> */}
+                                <Card.Img variant="top" src={'/Images/png/questionmark.png'} className='mx-auto mt-2' />
+                                <Card.Body className='p-1 '>
+                                    <div className='cardFlipper stretched-link' name={`${card.id}`} onClick={() => handleToggleCards(card)}>
+                                        <Card.Title className="memoryCardsTitle ">Click To Flip</Card.Title>
+                                    </div>
+                                </Card.Body>
+                            </div>
+                            <div className={`flip-card-back `}>
+                                <Card.Img variant="top" src={card.imageUrl} className='mx-auto mt-2' />
+                                <Card.Body className='p-1'>
+                                    <div className='cardFlipper stretched-link' name={`${card.id}`} onClick={() => handleToggleCards(card)}>
+                                        <Card.Title className="memoryCardsTitle ">{card.name}</Card.Title>
+                                    </div>
+                                </Card.Body>
+                            </div>
                         </Card>
                     </Col>
-                ))}
-            </Row>
+                ))
+                }
+            </Row >
         </>
     )
 }
